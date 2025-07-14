4f7a8102a7533da584f5b91d0aa0f92a3edc05e8
CooperCode7 <61284229+CooperCode7@users.noreply.github.com>
2025-07-13 20:05:21 -0500

    Add date column to be used for validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_14_010024) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_14_010347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_14_010024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "notes"
+    t.date "date"
     t.index ["form_id"], name: "index_responses_on_form_id"
     t.index ["user_id"], name: "index_responses_on_user_id"
   end
