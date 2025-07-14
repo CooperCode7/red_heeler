b78499ecd314d983c44c4b9ad5b09fb1047c3ead
CooperCode7 <61284229+CooperCode7@users.noreply.github.com>
2025-07-13 20:01:22 -0500

    Add validations to responses model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ApplicationRecord
   belongs_to :user
   belongs_to :form
+
+  validates :user_id, uniqueness: { scope: [ :form_id, :date ], message: "Has already submitted a response for this form today" }
 end
