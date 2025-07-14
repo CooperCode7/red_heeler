373b4c17d474b03f5c7dd45c6f15de948aa45b58
CooperCode7 <61284229+CooperCode7@users.noreply.github.com>
2025-07-13 19:28:59 -0500

    Add model tests for answer choices

--- test/models/answer_choice_test.rb
+++ test/models/answer_choice_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class AnswerChoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @question = Question.create!(content: "Sample question?", form: Form.first)
+  end
+
+  test "should be valid with option and question" do
+    answer_choice = AnswerChoice.new(option: "True", question: @question)
+    assert answer_choice.valid?
+  end
+
+  test "should be invalid without option" do
+    answer_choice = AnswerChoice.new(option: "", question: @question)
+    assert_not answer_choice.valid?
+    assert_includes answer_choice.errors[:option], "can't be blank"
+  end
+
+  test "should be invalid without question" do
+    answer_choice = AnswerChoice.new(option: "Maybe")
+    assert_not answer_choice.valid?
+    assert_includes answer_choice.errors[:question], "must exist"
+  end
+
+  test "position is optional" do
+    answer_choice = AnswerChoice.new(option: "False", question: @question)
+    assert answer_choice.position.nil?
+    assert answer_choice.valid?
+  end
 end
