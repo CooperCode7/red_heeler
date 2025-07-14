6e82057f5661dacc2c44ceef705fd18449469d98
CooperCode7 <61284229+CooperCode7@users.noreply.github.com>
2025-07-13 19:37:48 -0500

    Add validations to answer choices model

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -1,4 +1,7 @@
 class AnswerChoice < ApplicationRecord
   belongs_to :question
   default_scope { order(:position) }
+
+  validates :option, presence: true
+  validates :position, numericality: { only_integer: true }, allow_nil: true
 end
