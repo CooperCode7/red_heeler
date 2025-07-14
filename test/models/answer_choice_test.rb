16ff42ee8ffda6689ce296ee7ead1b0e847586b2
CooperCode7 <61284229+CooperCode7@users.noreply.github.com>
2025-07-13 19:38:00 -0500

    Add answr choices tests around position

--- test/models/answer_choice_test.rb
+++ test/models/answer_choice_test.rb
@@ -27,4 +27,20 @@ class AnswerChoiceTest < ActiveSupport::TestCase
     assert answer_choice.position.nil?
     assert answer_choice.valid?
   end
+
+  test "should belong to question" do
+    assoc = AnswerChoice.reflect_on_association(:question)
+    assert_equal :belongs_to, assoc.macro
+  end
+
+  test "should be invalid if position is not a number" do
+    answer_choice = AnswerChoice.new(option: "Maybe", position: "not_a_number", question: questions(:one))
+    assert_not answer_choice.valid?
+    assert_includes answer_choice.errors[:position], "is not a number"
+  end
+
+  test "should be valid if position is nil" do
+    answer_choice = AnswerChoice.new(option: "Maybe", position: nil, question: questions(:one))
+    assert answer_choice.valid?
+  end
 end
