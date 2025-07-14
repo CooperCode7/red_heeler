5e3b328644b60f75f7ce746ccd1935007107ed8f
CooperCode7 <61284229+CooperCode7@users.noreply.github.com>
2025-07-13 20:28:12 -0500

    Add new validation to responses model for checking that there is only one response per form in a given day

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,5 +2,26 @@ class Response < ApplicationRecord
   belongs_to :user
   belongs_to :form
 
-  validates :user_id, uniqueness: { scope: [ :form_id, :date ], message: "Has already submitted a response for this form today" }
+  validates :user_id, presence: true
+  validates :form_id, presence: true
+
+  validate :one_response_per_day
+
+  private
+
+  def one_response_per_day
+    timestamp = created_at || Time.zone.now
+    start_of_day = timestamp.beginning_of_day
+    end_of_day = timestamp.end_of_day
+
+    duplicate_exists = Response.where(
+      user_id: user_id,
+      form_id: form_id,
+      created_at: start_of_day..end_of_day
+      ).where.not(id: id).exists?
+
+    if duplicate_exists
+      errors.add(:base, "You can only submit one response per form per day")
+    end
+  end
 end
